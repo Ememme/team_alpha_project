a3958931cc74549a9ec474d150343e81bb6233e7
Adam <adamosch@gmail.com>
2017-08-11 13:36:46 +0200

    Fix column names

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe User do
   describe 'database columns' do
   	it { should have_db_column :book_unique_id }
   	it { should have_db_column :owner_id }
-  	it { should have_db_column :current_borrower_id }
+  	it { should have_db_column :borrower_id }
   	it { should have_db_column :title }
     it { should have_db_column :author }
+    it { should have_db_column :description }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe User do
   describe 'validations' do
   	it { is_expected.to validate_presence_of :email }
-  	it { is_expected.to validate_presence_of :login }
+    it { is_expected.to validate_presence_of :encrypted_password }
   end
 
   describe 'database columns' do
+  	it { should have_db_column :encrypted_password}
   	it { should have_db_column :email }
   	it { should have_db_column :login }
   	it { should have_db_column :first_name }
