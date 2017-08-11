9bd568b6295482fefe761b56ffd353a382043c6c
Adam <adamosch@gmail.com>
2017-08-11 12:42:07 +0200

    Add user specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'validations' do
+  	it { is_expected.to validate_presence_of :email }
+  	it { is_expected.to validate_presence_of :login }
+  end
+
+  describe 'database columns' do
+  	it { should have_db_column :email }
+  	it { should have_db_column :login }
+  	it { should have_db_column :first_name }
+  	it { should have_db_column :last_name }
+  end
+
+  describe 'associations' do
+  	it { is_expected.to have_many :books }
+  end
+end
