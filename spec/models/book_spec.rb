b43789848b0c749a79fb5c93ef8f71149bc80dba
Adam <adamosch@gmail.com>
2017-08-11 15:26:13 +0200

    add missing specs for books

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe User do
+RSpec.describe Book do
   describe 'validations' do
-  	it { is_expected.to validate_presence_of :title }
-  	it { is_expected.to validate_presence_of :author }
   end
 
   describe 'database columns' do
