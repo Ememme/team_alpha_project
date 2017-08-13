f870d5a6b35b8b8b210a55235c483db437cf75b2
Adam <adamosch@gmail.com>
2017-08-13 13:02:40 +0200

    Add book routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
 
-  get 'books/show'
-
-  get 'books/create'
-
-  get 'books/new'
-
-  get 'books/delete'
+  resources :books, only: [:index, :show]
 
   #get 'welcome/index'
 
