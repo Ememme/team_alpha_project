557dbee0817d7b205140f3b0f05cbec9d36fe027
Adam <adamosch@gmail.com>
2017-08-13 13:03:32 +0200

    Create books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,37 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show destroy]
+
   def index
+    @books = Book.all
   end
 
-  def show
-  end
+  def show; end
 
   def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: "Book was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
   end
+
+  def destroy; end
 
   def new
   end
 
-  def delete
-  end
+  private
+
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :owner_id, :borrower_id, :book_uid)
+    end
+
 end
