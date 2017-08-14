3a33a355da260a5c085f743bc81709cfa9c2fc32
Adam <adamosch@gmail.com>
2017-08-14 18:28:57 +0200

    Add carrierwave and cover to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   #belongs_to :borrower, class_name: :User, foreign_key: :borrower_id, optional: true
   belongs_to :user, class_name: :User
   has_many :loans
+  mount_uploader :cover, CoverUploader
 
   # def book_status
   #   if borrower_id == nil
