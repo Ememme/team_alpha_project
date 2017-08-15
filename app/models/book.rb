108eae74ce12e63a9e1f4b53c31362017c35aad7
joasiacode <consilis@wp.pl>
2017-08-15 14:56:18 +0200

    Book model validates

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,9 @@ class Book < ApplicationRecord
   belongs_to :user, class_name: :User
   has_many :loans
 
+  validates :title, presence: { message: "can't be blank" }
+  validates :author, presence: { message: "can't be blank" }
+
   acts_as_votable
 
 
