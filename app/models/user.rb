d2ac6881c7cf94a8e8b7e4d8b8f1b7b7a83fdade
Adam <adamosch@gmail.com>
2017-08-17 11:34:47 +0200

    Add currently borrowed method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   # TODO validates :login, presence: { message: "can't be blank" }
 
   acts_as_votable
+
+  def currently_borrowed
+  	loans.where(status: Book::STATUS_BORROWED).order(created_at: :desc).uniq { |l| l.book_id }
+  end
 end
